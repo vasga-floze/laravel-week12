98589cc860d2cadd5352e1387ca8bad949fcbdf2
@vasga-floze <gabriela.floze@gmail.com>
2021-04-11 04:34:33 -0600

    Se centra contenido de las vistas, agrega algunos estilos. Agrega barra de navegación.

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -1,28 +1,39 @@
 <!--AQUI DEBE IR EL FORMULARIO MEDIANTE EL CUAL SE VA A CREAR Y EDITAR UN PRODUCTO-->
-@section('title')
-FORMULARIO 
+<div class="text-center">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{ url('product') }}">INICIO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</div>
+
+@section('formulario')
+<div class="card">
+    <div class="card-body">
+        <form action="{{ url('/product') }}" method="post">
+            @csrf 
+
+            <label for="name">Nombre</label><br>
+            <input type="text" name="name" id="name" class="form-control" value=" {{ isset($product->name)?$product->name:'' }} "><hr/>
+
+
+            <label for="description">Descripcion</label><br>
+            <input class="form-control" type="text" name="description" id="description" value=" {{ isset($product->description)?$product->description:'' }} "><hr/>
+
+
+            <label for="price">Precio</label><br>
+            <input type="text" name="price" id="price" class="form-control" value=" {{ isset($product->price)?$product->price:'' }} "><hr/>
+
+            <label for="age">Edad recomendada</label><br>
+            <input type="text" name="age" id="age" class="form-control" value=" {{ isset($product->age)?$product->age:'' }} "><hr/>
+
+            <label for="modelNumber">Numero de modelo</label><br>
+            <input type="text" name="modelNumber" id="modelNumber" class="form-control" value=" {{ isset($product->modelNumber)?$product->modelNumber:'' }} "><hr/>
+
+            <button type="submit" class="btn btn-primary">Guardar</button><br>
+            <a href="{{ url('product') }}">Regresar</a>
+        </form>
+    </div>
+</div>
 @endsection
-
-
-<form action="{{ url('/product') }}" method="post">
-@csrf 
-
-<label for="name">Nombre</label>
-<input type="text" name="name" id="name" value=" {{ isset($product->name)?$product->name:'' }} "><hr/>
-
-<label for="description">Descripcion</label>
-<input type="text" name="description" id="description" value=" {{ isset($product->description)?$product->description:'' }} "><hr/>
-
-<label for="price">Precio</label>
-<input type="text" name="price" id="price" value=" {{ isset($product->price)?$product->price:'' }} "><hr/>
-
-<label for="age">Edad recomendada</label>
-<input type="text" name="age" id="age" value=" {{ isset($product->age)?$product->age:'' }} "><hr/>
-
-<label for="modelNumber">Numero de modelo</label>
-<input type="text" name="modelNumber" id="modelNumber" value=" {{ isset($product->modelNumber)?$product->modelNumber:'' }} "><hr/>
-
-<button type="submit">Guardar</button>
-
-<a href="{{ url('product') }}">Regresar</a>
-</form>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,43 +1,65 @@
 @extends('layouts.app')
+<div class="text-center">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{ url('product') }}">INICIO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('product/create') }}">Crear registro</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
 
 @section('title')
-LISTADO DE PRODUCTOS <br>
-<a href="{{ url('product/create') }}">Agregar</a>
+    LISTADO DE PRODUCTOS
+    <br>
+    <a href="{{ url('product/create') }}">Agregar producto</a>
 @endsection
 
 
-    <table class="table">
+@section('tabla')
+    <table class="table table-hover">
         <thead>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Descripcion</th>
-            <th>Precio</th>
-            <th>Edad recomendada</th>
-            <th>Numero de modelo</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Descripcion</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Edad recomendada</th>
+            <th scope="col">Numero de modelo</th>
+            <th scope="col">Editar</th>
+            <th scope="col">Eliminar</th>
         </thead>
         <tbody>
         @foreach( $products as $product)
             <tr>
-                <td> {{ $product->id}} </td>
+                <td scope="row"> {{ $product->id}} </td>
                 <td> {{ $product->name}} </td>
                 <td> {{ $product->description}} </td>
                 <td> {{ $product->price}} </td>
                 <td> {{ $product->age}} </td>
                 <td> {{ $product->modelNumber}} </td>
+                <!--Editar-->
+                <td>
+                    <a href="{{ url('/product/'.$product->id.'/edit') }}">Editar</a>
+                </td>
 
-                <td><a href="{{ url('/product/'.$product->id.'/edit') }}">Editar</a></td>
                 <!--Eliminar-->
                 <form method='POST' action="{{ url('/product/'.$product->id) }}">
                     @csrf
                     {{ method_field('DELETE') }} <!--define el metodo que es necesario para eliminar-->
                     <td>
-                        <input type="submit" onclick="return confirm('Desea eliminar el registro?')" value="Eliminar">
+                        <input class="btn btn-primary" type="submit" onclick="return confirm('Desea eliminar el registro?')" value="Eliminar">
                     </td>
                 </form>
             </tr>
         @endforeach
         </tbody>
 </table>
+@endsection
 
