29dc6dfd59f4896ab739845b23c15b9a51fdb74b
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-04-11 22:16:53 -0600

    Se agrega comentario

--- resources/views/product/acercade.blade.php
+++ resources/views/product/acercade.blade.php
@@ -13,11 +13,12 @@
 </div>
 
 
+<!--Se crean las tarjetas para mostrar el avatar y informacion de los estudiantes-->
 <div class="container card-group" style="width: 50rem;">
     <div class="card" style="width: 18rem;">
         <img src="{{asset('images/avatarKilmar.png')}}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Integrante</h5>
+          <h5 class="card-title">Estudiante</h5>
           <p class="card-text">Roberto Kilmar Chevez Reyes</p>
         </div>
       </div>
@@ -25,7 +26,7 @@
       <div class="card" style="width: 18rem;">
         <img src="{{asset('images/avatarVasti.png')}}"" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Integrante</h5>
+          <h5 class="card-title">Estudiante</h5>
           <p class="card-text">Vasti Gabriela Flores Zelaya</p>
         </div>
       </div>
